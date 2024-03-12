f59af22220d465ad6d40795f5de0535a52f8aabf
b33rk <brkmanics@gmail.com>
2024-03-12 20:07:55 +0700

    base

--- algo.py
+++ algo.py
@@ -4,32 +4,29 @@ def findMiddlePoint(point1: tuple[float, float], point2: tuple[float, float]) ->
 
     return middlePoint
 
-def addListOfPoint(idx: int, end : int, listPoint: list[tuple[float, float]], isDel : bool):
-    if(idx == end):
-        if (isDel and idx > 2):
-            listPoint = [x for x in listPoint if x != listPoint[idx - 2]]
-        print(isDel, idx, listPoint)
+def addListOfPoint(idx: int, listPoint: list[tuple[float, float]]):
+    if(idx == len(listPoint)-1):
+        # if (isDel or isEnd):
+        #     listPoint = [x for x in listPoint if x != listPoint[idx - 1]]
+        # print(isDel, idx, listPoint)
         return listPoint
     else:
-        if (isDel and idx > 2):
-            listPoint = [x for x in listPoint if x != listPoint[idx - 1]]
-            idx -= 1
+        # if (isDel and idx > 1):
+        #     listPoint = [x for x in listPoint if x != listPoint[idx - 1]]
+        #     idx -= 1
         newPoint = findMiddlePoint(listPoint[idx], listPoint[idx + 1])
         listPoint.insert(idx+1,newPoint)
-        print(isDel, end, idx, listPoint)
-        isDel = not isDel
-        return addListOfPoint(idx + 2, end, listPoint, isDel)
-
-
+        # print(len(listPoint)-1, idx, listPoint)
+        return addListOfPoint(idx + 2, listPoint)
 
 def findListOfPoint(iterasi : int, listPoint: list[tuple[float, float]]) -> list[tuple[float, float]]:
-    print("a")
-    if(iterasi == 0.5):
-        listPoint = addListOfPoint(1, len(listPoint) - 1, listPoint, True)
+    print("a", iterasi)
+    if(iterasi == 1):
+        listPoint = addListOfPoint(0, listPoint)
         return listPoint
     else :
-        listPoint = addListOfPoint(0, len(listPoint) - 1, listPoint, True)
-        return findListOfPoint(iterasi-0.5, listPoint)
+        listPoint = addListOfPoint(0, listPoint)
+        return findListOfPoint(iterasi-1, listPoint)
 
 
 
@@ -40,5 +37,5 @@ middlePoint12: tuple[float, float] = findMiddlePoint(point1, point2)
 
 pairOfPoint = [(0, 0), (2,2), (0, 4)]
 
-ans: list[tuple[float, float]] = findListOfPoint(1, pairOfPoint) 
+ans: list[tuple[float, float]] = findListOfPoint(2, pairOfPoint) 
 print(ans)
