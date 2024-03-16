8fd45a7b9d3ccda2d8464d53b9b784b7ac1f81da
b33rk <brkmanics@gmail.com>
2024-03-16 14:06:10 +0700

    fix

--- algo4.py
+++ algo4.py
@@ -1,10 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import time
 
 def findMiddlePoint(point1: tuple[float, float], point2: tuple[float, float]) -> tuple[float, float]:
-    middlePoint: tuple[float, float]
-    middlePoint = ((0.5) * point1[0] + 0.5 * point2[0], (0.5) * point1[1] + 0.5*point2[1])
-
-    return middlePoint
+    return ((0.5) * point1[0] + 0.5 * point2[0], (0.5) * point1[1] + 0.5*point2[1])
 
 def makeMiddlePoint(listPoint: list[tuple[float, float]]):
     middlePoint : list[tuple[float, float]] = []
@@ -32,27 +31,27 @@ def addListOfPoint(isLeft: bool, iterasi: int, listPoint: list[tuple[float, floa
         rightSide.reverse()
         rightSide.append(listPoint[-1])
         return addListOfPoint(True, iterasi - 1, leftSide) + addListOfPoint(False, iterasi - 1, rightSide)
-    
-
-point1: tuple[float, float] = (1, 0)
-point2: tuple[float, float] = (2, 0)
-middlePoint12: tuple[float, float] = findMiddlePoint(point1, point2)
-
 
 # pairOfPoint = [(1, 1), (3,1), (1, 2), (2,3)]
 # pairOfPoint = [(1, 2), (3,1), (3, 3), (0,1)]
-pairOfPoint = [(3, 1), (1,3), (3, 3), (1,1)]
+# pairOfPoint = [(3, 1), (1,3), (3, 3), (1,1)]
 # pairOfPoint = [(1, 1), (3,2), (1, 2), (3,3)]
+pairOfPoint = [(1, 1), (1,3), (0, 0), (3,1)]
 # pairOfPoint = [(2, 0), (0,2), (5, 2), (7,0), (10, 10)]
 # pairOfPoint = [(1, 1), (3,1), (1, 2), (2,3), (2,5), (9,0), (10,8), (7,3), (0,0), (4,5)]
 
 a : list[list[tuple[float, float]]] = []
-# ans: list[tuple[float, float]] = addListOfPoint(True, 7, pairOfPoint)
-# x, y = zip(*ans)
-# plt.plot(x, y, marker='o', linestyle='-', markersize=2)
-for i in range(1,5):
-    x, y = zip(*addListOfPoint(True, i, pairOfPoint))
-    plt.plot(x, y ,marker='o', linestyle='-', markersize=5, label = i + 1) 
+start_time = time.time()
+ans: list[tuple[float, float]] = addListOfPoint(True, 20, pairOfPoint)
+end_time = time.time()
+execution_time = end_time - start_time
+print(len(ans))
+print("Execution time:", execution_time, "seconds")
+x, y = zip(*ans)
+plt.plot(x, y, marker='o', linestyle='-', markersize=2)
+# for i in range(1,5):
+#     x, y = zip(*addListOfPoint(True, i, pairOfPoint))
+#     plt.plot(x, y ,marker='o', linestyle='-', markersize=5, label = i + 1) 
 x, y = zip(*pairOfPoint)
 plt.plot(x, y, marker='o', linestyle='-', markersize=5) 
 plt.title('Plot of Lists of Points')
@@ -60,14 +59,3 @@ plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.grid(True)
 plt.show()
-
-# def DnC_Point(middlePoint:  list[list[tuple[float, float]]], Point1: tuple[float, float], Point2: tuple[float, float], tempPoint : list[list[tuple[float, float]]]):
-#     medList = [x[0] for x in middlePoint]
-#     tempList = [Point1] + medList + [Point2]
-#     index = 0
-#     while (len(tempPoint) != len(tempList) - 1):
-#         tempPoint.append(makeMiddlePoint(tempList))
-#         tempList = tempPoint[index]
-#         index += 1
-#     point = DnC_MiddlePoint(Point1, Point2, tempPoint[-1][0])
-#     return [Point1] + [point] + [Point2]
---
+++ bruteForc.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import time
+
+Point = tuple[float, float] 
+
+def bezierCurveNPoint(list_point: list[Point], smoother: float = 0.1) -> list[Point]:
+    n: int = len(list_point) - 1
+    result: list[Point] = []
+    t: float = 0
+    while(t <= 1):
+        x: float = 0
+        y: float = 0
+        for k, (point_x, point_y) in enumerate(list_point): 
+            x += bezierPointCalc(point_x, n, k, t)
+            y += bezierPointCalc(point_y, n, k, t)
+        result.append((x, y)) 
+        t += smoother
+    return result
+
+def bezierPointCalc(point_x_or_y: float, n: int, k: int, t: float) -> float:
+    return binomialCoeff(n, k) * point_x_or_y * pow((1-t), (n-k)) * pow(t, k)
+
+def binomialCoeff(n: int, k: int) -> float: 
+    return fact(n) / (fact(k) * fact(n-k))
+
+def fact(n: int) -> int: 
+    ans: int = 1
+    for i in range(1, n + 1):
+        ans *= i
+    return ans
+    
+def plotBezier(core_point: list[Point], curve_point: list[Point]):
+    x_core, y_core = zip(*core_point)
+    x_curve, y_curve = zip(*curve_point)
+    plt.plot(x_core, y_core, marker = 'x', linestyle = '-', label = 'Initial Point')
+    plt.plot(x_curve, y_curve, marker='o', linestyle='-', label = 'Bezier Curve')
+
+    plt.title('Bezier Curve on Cartesian Diagram')
+    plt.xlabel('X-axis')
+    plt.ylabel('Y-axis')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+list_point = [(0, 0), (2, 2), (4, 0), (3, 4), (5, 6), (6,-5), (7, 10), (8, -10), (10, 10), (11, -50), (50, -100)]
+list_point1 = [(2, 0), (0,2), (5, 2), (7,0), (10, 10)]
+start_time = time.time()
+points = bezierCurveNPoint(list_point1, 0.00064)
+end_time = time.time()
+execution_time = end_time - start_time
+print(len(points))
+print("Execution time:", execution_time, "seconds")
+plotBezier(list_point1, points)
